488b06cb9887ac199909a07232b5969e6c5f5d1e
Marco Centurión <mcenturion@protonmail.com>
2019-10-03 17:58:18 -0300

    Change uwsgi.sock location
    Centos7's /tmp is namespaced, which made it impossible for the nginx
    process to read the socket. Changed it to a more appropiate location
    on /run/uwsgi.sock

--- scripts/install_mhnserver.sh
+++ scripts/install_mhnserver.sh
@@ -101,7 +101,7 @@ server {
 
     location @mhnserver {
       include uwsgi_params;
-      uwsgi_pass unix:/tmp/uwsgi.sock;
+      uwsgi_pass unix:/run/uwsgi.sock;
     }
 
     location  /static {
@@ -113,7 +113,7 @@ EOF
 
 cat > /etc/supervisor/conf.d/mhn-uwsgi.conf <<EOF 
 [program:mhn-uwsgi]
-command=$MHN_HOME/env/bin/uwsgi -s /tmp/uwsgi.sock -w mhn:mhn -H $MHN_HOME/env --chmod-socket=666 -b 40960
+command=$MHN_HOME/env/bin/uwsgi -s /run/uwsgi.sock -w mhn:mhn -H $MHN_HOME/env --chmod-socket=666 -b 40960
 directory=$MHN_HOME/server
 stdout_logfile=/var/log/mhn/mhn-uwsgi.log
 stderr_logfile=/var/log/mhn/mhn-uwsgi.err
